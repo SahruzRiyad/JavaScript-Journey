13c29d8b4f2cbc3c3164c6b66e92f83daee36aff
SahruzRiyad <sahruzzaman01@gmail.com>
2025-07-31 23:11:48 +0600

    edited task to and fixed scroll down section

--- Task_31_July_2025/task-2/selectMiddle.js
+++ Task_31_July_2025/task-2/selectMiddle.js
@@ -1,42 +1,67 @@
 (function () {
-    const selectedValues = { monthly: null, once: null };
+
+    const formsState = []; 
     const defaultIndex = 1;
 
-    function applySelectionFor(type, selector, defaultIndex) {
-        const radios = document.querySelectorAll(selector);
+    function applySelectionFor(type, selector, form, formIndex) {
+
+        const radios = form.querySelectorAll(selector);
 
         if (radios.length > 0) {
-            if (selectedValues[type]) {
+
+            if (!formsState[formIndex]) {
+                formsState[formIndex] = { monthly: null, once: null };
+            }
+
+            const selectedId = formsState[formIndex][type];
+
+            if (selectedId && form.querySelector(`#${selectedId}`)) {
                 radios.forEach(radio => {
-                    radio.checked = (radio.id === selectedValues[type]);
+                    radio.checked = (radio.id === selectedId);
                 });
-            } else {
+            } 
+            else {
                 radios.forEach(radio => (radio.checked = false));
-                radios[defaultIndex].checked = true;
-                selectedValues[type] = radios[defaultIndex].id;
+                if (radios[defaultIndex]) {
+                    radios[defaultIndex].checked = true;
+                    formsState[formIndex][type] = radios[defaultIndex].id;
+                }
             }
         }
     }
 
-    function applySelection() {
-
-        applySelectionFor('monthly', 'input.donate__radio[id*="monthlyamount"]', defaultIndex);
-        applySelectionFor('once', 'input.donate__radio[id*="singleamount"]', defaultIndex);
+    function applyAllSelections() {
+        const donationForms = document.querySelectorAll('form[action*="/Giving/Donate"]');
+        
+        donationForms.forEach((form, index) => {
+            form.dataset.formIndex = index; 
+            
+            applySelectionFor('monthly', 'input.donate__radio[id*="monthlyamount"]', form, index);
+            applySelectionFor('once', 'input.donate__radio[id*="singleamount"]', form, index);
+        });
     }
 
     document.body.addEventListener('change', function (e) {
-        if (e.target.matches('input.donate__radio[id*="monthlyamount"]')) {
-            selectedValues.monthly = e.target.id;
-        }
-        if (e.target.matches('input.donate__radio[id*="singleamount"]')) {
-            selectedValues.once = e.target.id;
+        if (e.target.matches('input.donate__radio[id*="monthlyamount"], input.donate__radio[id*="singleamount"]')) {
+            const parentForm = e.target.closest('form[action*="/Giving/Donate"]');
+
+            if (parentForm && parentForm.dataset.formIndex) {
+                const formIndex = parseInt(parentForm.dataset.formIndex, 10);
+                
+                if (e.target.matches('input.donate__radio[id*="monthlyamount"]')) {
+                    formsState[formIndex].monthly = e.target.id;
+                }
+                if (e.target.matches('input.donate__radio[id*="singleamount"]')) {
+                    formsState[formIndex].once = e.target.id;
+                }
+            }
         }
     });
 
-    applySelection();
+    applyAllSelections();
 
     const observer = new MutationObserver(() => {
-        applySelection();
+        applyAllSelections();
     });
 
     observer.observe(document.body, { childList: true, subtree: true });
